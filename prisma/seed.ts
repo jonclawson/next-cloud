dce58bd66e23f75b675af59880a68fa6d986ffd5
jonclawson <30538332+jonclawson@users.noreply.github.com>
2026-01-10 15:52:14 -0800

    update prisma client path for seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
@@ -27,4 +27,4 @@ main()
   .catch((e) => {
     console.error(e);
     process.exit(1);
-  });
+  })
