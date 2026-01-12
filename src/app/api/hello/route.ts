0e30d109207766bce9e18b0ba7c095cd75d2d1b7
jonclawson <30538332+jonclawson@users.noreply.github.com>
2026-01-12 12:39:44 -0800

    update image logic

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -15,6 +15,7 @@ export async function GET() {
     const { env } = await getCloudflareContext();
     const key = "next.png";
     const object = await env?.NEXT_CLOUD_R2_BUCKET?.get(key);
+    const body = await object?.arrayBuffer();
     const [setting] = await db
       .select()
       .from(schema.settings)
@@ -24,15 +25,7 @@ export async function GET() {
     console.log('Setting:', setting);
     return NextResponse.json({
        message: `hello ${setting?.value}`, 
-       // base64 imaage data or placeholder text
-       image: object ? await object.arrayBuffer().then((buffer: any) => {
-         const uint8Array = new Uint8Array(buffer);
-         let binary = '';
-         for (let i = 0; i < uint8Array.byteLength; i++) {
-           binary += String.fromCharCode(uint8Array[i]);
-         }
-         return 'data:' + object.httpMetadata.contentType + ';base64,' + btoa(binary);
-       }) : ''
+      image: body ? `data:${object?.httpMetadata?.contentType || 'image/png'};base64,${Buffer.from(body).toString('base64')}` : null 
       });
   } catch (error) {
     console.error('Error fetching settings:', error);
