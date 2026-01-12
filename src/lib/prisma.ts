6c7433d9c16089ecd8d6db9eb15181765083d078
jonclawson <30538332+jonclawson@users.noreply.github.com>
2026-01-11 23:13:49 -0800

    prisma w prisma.io

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,27 @@
-// import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client/edge' // Use your custom output path
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-// Pass connection string directly to the adapter
-// const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
-
-// Instantiate the client with the adapter
-const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL! }).$extends(withAccelerate())
+const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL! })
+.$extends(withAccelerate())
 export default prisma
+
+//   Prisma Client could not locate the Query Engine for runtime "debian-openssl-1.1.x".
+// import { neonConfig } from '@neondatabase/serverless';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaNeon } from '@prisma/adapter-neon';
+// import ws from 'ws';
+
+// neonConfig.webSocketConstructor = ws;
+
+// const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from '@prisma/adapter-pg'
+
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+// const prisma = new PrismaClient({adapter})
+
+// export default prisma;
