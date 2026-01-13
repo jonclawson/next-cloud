5d3529123e8529730028cbafc9da7a65e478267f
jonclawson <30538332+jonclawson@users.noreply.github.com>
2026-01-12 19:19:48 -0800

    clean up prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,40 +1,11 @@
-// import { PrismaClient } from '@prisma/client/edge' // Use your custom output path
-// import { withAccelerate } from "@prisma/extension-accelerate";
-
-// const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL! })
-// .$extends(withAccelerate())
-// export default prisma
-
-//   Prisma Client could not locate the Query Engine for runtime "debian-openssl-1.1.x".
-// import { neonConfig } from '@neondatabase/serverless';
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaNeon } from '@prisma/adapter-neon';
-// import ws from 'ws';
-
-// neonConfig.webSocketConstructor = ws;
-
-// const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
-// const prisma = new PrismaClient({ adapter });
-
-// export default prisma;
-
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from '@prisma/adapter-pg'
-
-// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-// const prisma = new PrismaClient({adapter})
-
-// export default prisma;
 
 import { PrismaClient } from '@prisma/client/edge'
 import { PrismaPg } from '@prisma/adapter-pg'
-// import { Pool } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-// const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
